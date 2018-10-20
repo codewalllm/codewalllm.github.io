63e95947738dedf170cc7f40984b90a1313d16a2
codewalllm <861775452@qq.com>
2018-10-20 10:42:30 +0800

    添加内容

--- README.md
+++
@@ -1,1 +0,0 @@
-# codewalllm.github.io
---
+++ static/RequestConfig.js
@@ -0,0 +1,19 @@
+/**
+ * 修改说明:
+ */
+var reqCfg = {
+  requestUrl: "http://testaudio.bjjypay.com.cn:8601/",
+  // requestUrl: 'http://127.0.0.1:8888/'
+  webUrl: "http://chenx.bjjypay.com.cn/marketingweb/",
+  appId: "wx00f2b5f583cbe54b"
+};
+var shareCfg = {
+  imgUrl: reqCfg.webUrl + "static/img/ITZ.png",
+  title: "各类用卡提额养卡下卡技巧实操授课",
+  desc: "定期免费分享各类用卡提额养卡下卡技巧，还有各类，薅羊毛活动实时分享，公开实操授课等。"
+};
+var qrCfg = {
+  imgUrl: `${reqCfg.webUrl}static/img/platform.png`,
+  platform: "北极狐狸",
+  desc: "正确的使卡用卡养卡技巧"
+};
