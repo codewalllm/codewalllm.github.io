ee19ffd957391131b03c33674b48df00a355441d
codewalllm <861775452@qq.com>
2024-04-09 22:56:28 +0800

    测试demo文件部署

---
+++ githubdemo/js/about.2fbc6a7f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkgithubdemo"]=self["webpackChunkgithubdemo"]||[]).push([[594],{603:function(u,n,t){t.r(n),t.d(n,{default:function(){return b}});var e=t(6768);const a={class:"about"},r=(0,e.Lk)("h1",null,"This is an about page",-1),s=[r];function c(u,n){return(0,e.uX)(),(0,e.CE)("div",a,s)}var i=t(1241);const o={},h=(0,i.A)(o,[["render",c]]);var b=h}}]);
+//# sourceMappingURL=about.2fbc6a7f.js.map
